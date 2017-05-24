6407cc835d9b45d5c7bf546a2339d11bdc37ee84
michalleoniec <michalleoniec@gmail.com>
2017-05-24 22:14:19 +0200

    Dodano do galerii: miniatury, slider, film Poprawiono responsywność: galeria, kontakt
    Wciaż nie działa: formularz kontaktowy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import {Routes, RouterModule}        from '@angular/router';
 import {HomeComponent}               from './home/home.component';
 import {FunctionsComponent}          from './functions/functions.component';
 import {GalleryComponent}            from './gallery/gallery.component';
+import {ContactComponent}            from './contact/contact.component';
 // import {HomeComponent}            from './home/home.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'functions', component: FunctionsComponent},
-  {path: 'gallery', component: GalleryComponent}
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'contact', component: ContactComponent}
 
 ];
 
---
+++ src/assets/js/galleryslider.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Michał Leoniec on 24.05.2017.
+ */
+
+
+// jQuery(document).ready(function($) {
+
+$(document).ready(function(){
+  $('.my-slider').unslider();
+});
