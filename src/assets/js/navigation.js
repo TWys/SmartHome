8697cdf0f4333a3841dec6e3d3a31bdc0a15616b
pawell67 <pawellwankiewicz@gmail.com>
2017-05-30 20:51:28 +0200

    poprawki

--- src/assets/js/navigation.js
+++ src/assets/js/navigation.js
@@ -11,3 +11,15 @@ $('a[href^="#"]').on('click', function (event) {
   }
 });
 
+$(document).ready(function () {
+  $('.nav li a').click(function(e) {
+
+    $('.nav li').removeClass('active');
+
+    var $parent = $(this).parent();
+    if (!$parent.hasClass('active')) {
+      $parent.addClass('active');
+    }
+    e.preventDefault();
+  });
+});
