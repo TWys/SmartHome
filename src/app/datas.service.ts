c065f631dc9c3454992907e7e5ceaff2ec3f66cf
TWysocki <tomaszwgdynia@wp.pl>
2017-06-19 22:11:19 +0200

    Sterowanie temperaturą- kontynuacja

--- src/app/datas.service.ts
+++ src/app/datas.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs/Observable";
 import {Datas} from "./datas";
-import {Http, BaseRequestOptions, Response, ResponseOptions, RequestMethod, RequestOptions} from '@angular/http';
+import {Http, Headers, BaseRequestOptions, Response, ResponseOptions, RequestMethod, RequestOptions} from '@angular/http';
 import {MockBackend, MockConnection} from '@angular/http/testing';
 
 @Injectable()
@@ -9,7 +9,9 @@ export class DatasService {
 
   public response = '{"temperature": [{"actual": 25,"requested": 26}, {"actual": 24.5,"requested": 24}, {"actual": 18.9,"requested": 24},{"actual": 17.5,"requested": 24}, {"actual": 28.4,"requested": 24}, {"actual": 30.0,"requested": 24}, {"actual": 21.8,"requested": 24}, {"actual": 16.9,"requested": 24},{"actual": 27.1,"requested": 24}, {"actual": 16,"requested": 24}],"humidity": [{"actual": 60}, {"actual": 55}, {"actual": 57}, {"actual": 51}, {"actual": 48}]}';
 
-  TABLE_A_URL: string = 'http://api.nbp.pl/api/cenyzlota/2017-06-06?format=json';
+  // TABLE_A_URL: string = 'http://api.nbp.pl/api/cenyzlota/2017-06-06?format=json';
+  // TABLE_A_URL: string = 'http://jsonplaceholder.typicode.com/posts';
+  TABLE_A_URL: string = 'http://www.smhome.pl';
   private wartosc;
 
 
@@ -25,6 +27,7 @@ export class DatasService {
     if (this.datas.requeted_temp < 18) this.datas.requeted_temp = 18;
     else if (this.datas.requeted_temp > 40) this.datas.requeted_temp = 40;
 
+    // this.fHttpTest('temp', this.datas.requeted_temp);
     return this.datas.requeted_temp;
   }
 
@@ -36,10 +39,19 @@ export class DatasService {
     return this.datas.actual_temp;
   }
 
-  fHttpTest() {
-    this.http.get(this.TABLE_A_URL)
-      .map(res => res.json())
-      .subscribe(data => console.log(data[0].cena));
+  fHttpTest(name?: string, value?: number) {
+    // let username: string = 'serwer1734514';
+    // let password: string = 'Projekt2017';
+
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    // let dane = 'rolUp=' +1;
+
+    return this.http.post(this.TABLE_A_URL, {title: 'foo', body: 'bar', userId: 1})
+    // this.http.get(this.TABLE_A_URL)
+    //   .map(res => res.json())
+      .subscribe(data => console.log(data));
     //  .subscribe (data => this.datas.actual_temp = data[0].cena);
     //return this.datas.actual_temp;
   }
@@ -47,10 +59,12 @@ export class DatasService {
   fGetHumidity() {
     var x: number = Math.floor(Math.random() * (4 - 0 + 1) + 0);
     this.datas.humidity = JSON.parse(this.response).humidity[x].actual;
+    //this.fHttpTest();
     return this.datas.humidity;
   }
 
   fGetLightStatus(x) {
+    //this.fHttpTest();
     if (x == 0) {
       document.getElementById('bulb').style.fill = 'black';
       return 0;
@@ -62,6 +76,7 @@ export class DatasService {
   }
 
   fSetBlinds(x) {
+    //this.fHttpTest();
     if (x == 0) return "../../assets/img/gif/blinds_down.gif";
     else if (x == 1) return "../../assets/img/gif/blinds_up.gif";
   }
